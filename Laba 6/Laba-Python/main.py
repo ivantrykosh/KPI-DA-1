f257c9ef03e0db4fa00c809339463575c3ce1e49
Ivan Trykosh <vanytrykoch789@gmail.com>
2022-12-18 16:56:31 +0200

    Add minimax algorithm

--- Laba 6/Laba-Python/main.py
+++ Laba 6/Laba-Python/main.py
@@ -33,10 +33,13 @@ class Game():
         self.size = len(GAME_FIELD)
         self.game_field = GAME_FIELD
         self.colors = COLORS
-        self.players = [Player('Player 1', self.colors[0]), Player('Player 2', self.colors[1])]
+        self.players = [Player('Player 1 (PC)', self.colors[0]), Player('Player 2 (user)', self.colors[1])]
         self.players_number = len(self.players)
         self.index_current_player = self.set_first_player()
         self.winner = None
+
+        self.max_score = self.size ** 2
+        self.min_score = -self.max_score
         return
 
     def set_first_player(self):
@@ -67,7 +70,7 @@ class Game():
         return True
 
     def is_turn(self, index_player, cell):
-        """Перевірка, чи може гравець походити у цю клітинку (cell - список або множина у вигляді [i, j] або (i, j)"""
+        """Перевірка, чи може гравець походити у цю клітинку (cell - список або множина у вигляді [i, j] або (i, j))"""
         color_op = 'blue' if self.players[index_player].color == 'red' else 'red'
         if cell[0] - 1 != -1:
             if self.game_field[cell[0] - 1][cell[1]] == color_op:
@@ -100,27 +103,157 @@ class Game():
                 print(color, j[0], '\033[00m', sep='', end='')
             print()
         return
+    '''------------------------------------------------------'''
+    def is_move(self, field, index_player, cell):
+        """Перевірка, чи може гравець походити у цю клітинку (cell - список або множина у вигляді [i, j] або (i, j))"""
+        color_op = 'blue' if self.players[index_player].color == 'red' else 'red'
+        if cell[0] - 1 != -1:
+            if field[cell[0] - 1][cell[1]] == color_op:
+                return False
+        if cell[0] + 1 != self.size:
+            if field[cell[0] + 1][cell[1]] == color_op:
+                return False
+        if cell[1] - 1 != -1:
+            if field[cell[0]][cell[1] - 1] == color_op:
+                return False
+        if cell[1] + 1 != self.size:
+            if field[cell[0]][cell[1] + 1] == color_op:
+                return False
+        return True
+
+    def is_looser(self, field, index_player):
+        """Перевірка, чи програв гравець"""
+        index_opp = (index_player + 1) % self.players_number
+        for i in range(self.size):
+            for j in range(self.size):
+                if not field[i][j] == '-':
+                    if field[i][j] == '0':
+                        if self.is_move(field, index_player, [i, j]):
+                            return False
+        return True
+
+    def minimax(self, field, depth, index_player):
+        """Мінімакс"""
+        result = self.is_looser(field, index_player)
+
+        if result:
+            if not index_player:
+                return self.min_score + depth
+            else:
+                return self.max_score - depth
+
+        if not index_player:
+            best_score = -float('inf')
+
+            for i in range(self.size):
+                for j in range(self.size):
+                    if field[i][j] == '0':
+                        if self.is_turn(index_player, [i, j]):
+                            field[i][j] = self.players[index_player].color
+                            index_next_player = (index_player + 1) % 2
+                            best_score = max(best_score, self.minimax(field, depth + 1, index_next_player))
+                            field[i][j] = '0'
+
+            return best_score
+        else:
+            best_score = float('inf')
+
+            for i in range(self.size):
+                for j in range(self.size):
+                    if field[i][j] == '0':
+                        if self.is_turn(index_player, [i, j]):
+                            field[i][j] = self.players[index_player].color
+                            index_next_player = (index_player + 1) % 2
+                            best_score = min(best_score, self.minimax(field, depth + 1, index_next_player))
+                            field[i][j] = '0'
+
+            return best_score
+        return
+
+    def find_best_move(self, field, index_player):
+        """Знаходження найкращого ходу"""
+        best_score = float('inf') if index_player else -float('inf')
+        best_move = None
+
+        for i in range(self.size):
+            for j in range(self.size):
+                if field[i][j] == '0':
+                    if self.is_move(field, index_player, [i, j]):
+                        field[i][j] = self.players[index_player].color
+                        index_next_player = (index_player + 1) % 2
+                        move_score = self.minimax(field, 0, index_next_player)
+                        field[i][j] = '0'
+
+                        if not index_player:
+                            if move_score > best_score:
+                                best_score = move_score
+                                best_move = [i, j]
+                        else:
+                            if move_score < best_score:
+                                best_score = move_score
+                                best_move = [i, j]
+
+        return best_score, best_move
 
 def main():
     """Початок гри"""
-    game = Game(GAME_FIELD2, COLORS)
+    GAME_FIELD = [['-', '0', '0', '-'],
+                  ['0', '0', '0', '0'],
+                  ['0', '0', '0', '0'],
+                  ['-', '0', '0', '-']]
+    # game = Game(GAME_FIELD2, COLORS)
+    #
+    # while True:
+    #     print('-'*100)
+    #     print('\033[36m', game.players[game.index_current_player].name, ' : ' , game.players[game.index_current_player].color, '\033[00m', sep='')
+    #     game.output()
+    #     numb = int(input('\033[93mInput the cell\'s number: \033[00m'))
+    #     i, j = numb // game.size, numb % game.size
+    #     if not (0 <= numb < game.size ** 2) or game.game_field[i][j] != '0' or not game.is_turn(game.index_current_player, [i, j]):
+    #         print('\033[91mThe number is wrong\033[00m')
+    #         continue
+    #     else:
+    #         game.set_cell([i, j], game.players[game.index_current_player].color)
+    #         if game.is_winner(game.index_current_player):
+    #             game.output()
+    #             print('\033[47m\033[90m', 'The winnner is: ', game.winner.name, ' (color: ', game.winner.color, ')', '\033[00m', sep='')
+    #             break
+    #         game.set_next_player()
+
+    # --------------------------------------------------------------------
+    game = Game(GAME_FIELD, COLORS)
 
     while True:
-        print('-'*100)
-        print('\033[36m', game.players[game.index_current_player].name, ' : ' , game.players[game.index_current_player].color, '\033[00m', sep='')
+        print('-' * 100)
+        print('\033[36m', game.players[game.index_current_player].name, ' : ',
+              game.players[game.index_current_player].color, '\033[00m', sep='')
         game.output()
-        numb = int(input('\033[93mInput the cell\'s number: \033[00m'))
-        i, j = numb // game.size, numb % game.size
-        if not (0 <= numb < game.size ** 2) or game.game_field[i][j] != '0' or not game.is_turn(game.index_current_player, [i, j]):
-            print('\033[91mThe number is wrong\033[00m')
-            continue
+        if game.index_current_player:
+            numb = int(input('\033[93mInput the cell\'s number: \033[00m'))
+            i, j = numb // game.size, numb % game.size
+            if not (0 <= numb < game.size ** 2) or game.game_field[i][j] != '0' or not game.is_turn(game.index_current_player, [i, j]):
+                print('\033[91mThe number is wrong\033[00m')
+                continue
+            else:
+                game.set_cell([i, j], game.players[game.index_current_player].color)
+                if game.is_winner(game.index_current_player):
+                    game.output()
+                    print('\033[47m\033[90m', 'The winnner is: ', game.winner.name, ' (color: ', game.winner.color, ')',
+                          '\033[00m', sep='')
+                    break
+                game.set_next_player()
         else:
-            game.set_cell([i, j], game.players[game.index_current_player].color)
+            best_score, best_move = game.find_best_move(game.game_field, game.index_current_player)
+            print('Best score for PC: ', best_score)
+            game.set_cell([best_move[0], best_move[1]], game.players[game.index_current_player].color)
             if game.is_winner(game.index_current_player):
+                game.winner = game.players[game.index_current_player]
                 game.output()
-                print('\033[47m\033[90m', 'The winnner is: ', game.winner.name, ' (color: ', game.winner.color, ')', '\033[00m', sep='')
+                print('\033[47m\033[90m', 'The winnner is: ', game.winner.name, ' (color: ', game.winner.color, ')',
+                      '\033[00m', sep='')
                 break
             game.set_next_player()
+
     return
 
 if __name__ == "__main__":
